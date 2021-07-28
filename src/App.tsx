22be3bf86df8fb3eaa75ec3981b785151419c6a2
Remy Chazelas <remychazelas@gmail.com>
2021-07-28 16:42:15 +0200

    app finish, not really functional

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Chessboard from './components/chessboard/Chessboard';
+
+function App() {
+  return (
+    <div id="app">
+      <Chessboard />
+    </div>
+  );
+}
+
+export default App;
